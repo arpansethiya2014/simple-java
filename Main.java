4fed1a580f8a2e1d14e048728717214685f468de
Arpan Sethiya <arpansethiya2014@gmail.com>
2024-05-10 01:05:42 +0530

    one more class added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.stream;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Arpan....!");
+	    System.out.println("Sum: " + 10+20);
+	    System.out.println("Date:" + new Date());
+	    System.out.println("Hello Arpan....exit!");
+	}
+}
